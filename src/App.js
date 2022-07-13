f612a916cab158fb4da2613a58445b3856d36844
shintaro-mizumoto <82919755+shintaro-mizumoto@users.noreply.github.com>
2022-07-13 17:01:33 +0900

    3-32

--- src/App.js
+++ src/App.js
@@ -1,36 +1,68 @@
 import React, { Component } from 'react'
 import './App.css'
 
+let theme = {
+  light:{
+    styles: {
+      backgroundColor:"#f0f9ff",
+      color:"#00f",
+    },
+    head: "bg-primary text-white display-4 mb-4",
+    alert: "alert alert-primary my-3",
+    text: "text-primary m-3",
+    foot: "py-4",
+  },
+  dark:{
+    styles: {
+      backgroundColor:"#336",
+      color:"#eef",
+    },
+    head: "bg-secondary text-white display-4 mb-4",
+    alert: "alert alert-dark my-3",
+    text: "text-light m-3",
+    foot: "py-4",
+  }
+}
+
+const ThemeContext = React.createContext(theme.light) //☆
+
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      counter:0, 
-      msg:'count start!',
-    }
-    this.doAction = this.doAction.bind(this)
-  }
+  static contextType = ThemeContext
 
-  doAction(event){
-    this.setState({
-      counter: this.state.counter + 1,
-      msg: '*** count: ' + this.state.counter + ' ***'
-    })
-  }
-
-  render(){
-    return  <div>
-      <h1 className="bg-primary text-white display-4">React</h1>  
+    render(){
+    return <div style={this.context.styles}>
+      <h1 className={this.context.head}>React</h1>
       <div className="container">
-        <p className="subtitle">Count number.</p>
-        <div className="alert alert-primary text-center">
-          <p className="h5 mb-4">{this.state.msg}</p>
-          <button className="btn btn-primary" 
-            onClick={this.doAction}>
-            Click</button>
-        </div>
+        <Title value="Content page" />
+        <Message value="This is Content sample." />
+        <Message value="※これはテーマのサンプルです。" />
+        <div className={this.context.foot}></div>
       </div>
     </div>
+  }
+}
+
+class Title extends Component {
+  static contextType = ThemeContext
+
+  render(){
+    return (
+      <div className={this.context.alert}>
+        <h2 style={this.context.style}>{this.props.value}</h2>
+      </div>
+    )
+  }
+}
+
+class Message extends Component {
+  static contextType = ThemeContext
+
+  render(){
+    return (
+      <div style={this.context.style}>
+        <p className={this.context.text}>{this.props.value}</p>
+      </div>
+    )
   }
 }
 
