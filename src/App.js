838f80899bc76eded19d0153496579644d6fc1f7
shintarou37 <m.sintarou37@gmail.com>
2022-07-13 22:20:43 +0900

    4-2

--- src/App.js
+++ src/App.js
@@ -1,69 +1,27 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-let theme = {
-  light:{
-    styles: {
-      backgroundColor:"#f0f9ff",
-      color:"#00f",
-    },
-    head: "bg-primary text-white display-4 mb-4",
-    alert: "alert alert-primary my-3",
-    text: "text-primary m-3",
-    foot: "py-4",
-  },
-  dark:{
-    styles: {
-      backgroundColor:"#336",
-      color:"#eef",
-    },
-    head: "bg-secondary text-white display-4 mb-4",
-    alert: "alert alert-dark my-3",
-    text: "text-light m-3",
-    foot: "py-4",
+function App() {
+  const [count, setCount] = useState(0)
+  const clickFunc = () => {
+    setCount(count + 1)
   }
-}
-
-const ThemeContext = React.createContext(theme.light) //☆
-
-class App extends Component {
-  static contextType = ThemeContext
-
-    render(){
-    return <div style={this.context.styles}>
-      <h1 className={this.context.head}>React</h1>
+  
+  return (
+    <div>
+      <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
-        <Title value="Content page" />
-        <Message value="This is Content sample." />
-        <Message value="※これはテーマのサンプルです。" />
-        <div className={this.context.foot}></div>
+        <h4 className="my-3">Hooks sample</h4>
+        <div className="alert alert-primary text-center">
+          <p className="h5 mb-3">click: {count} times!</p>
+          <div><button className="btn btn-primary"
+              onClick={clickFunc}>
+            Click me
+          </button></div>
+        </div>
       </div>
     </div>
-  }
-}
-
-class Title extends Component {
-  static contextType = ThemeContext
-
-  render(){
-    return (
-      <div className={this.context.alert}>
-        <h2 style={this.context.style}>{this.props.value}</h2>
-      </div>
-    )
-  }
-}
-
-class Message extends Component {
-  static contextType = ThemeContext
-
-  render(){
-    return (
-      <div style={this.context.style}>
-        <p className={this.context.text}>{this.props.value}</p>
-      </div>
-    )
-  }
+  )
 }
 
 export default App
