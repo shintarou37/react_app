c6fcc68fbea7ddbb89bd0a0a1a0e502fe4e09cb0
shintarou37 <m.sintarou37@gmail.com>
2022-07-14 23:05:34 +0900

    4-9

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
-function AlertMessage() {
+function AlertMessage(props) {
   return <div className="alert alert-primary h5 text-primary">
-    This is Alert message!
-  </div>
-}
-
-function CardMessage() {
-  return <div className="card p-3 h5 border-primary text-center">
-    This is Card message!
+    <h5>{props.msg}</h5>
   </div>
 }
 
 function App() {
+  const [val, setVal] = useState(0)
+  const [msg, setMsg] = useState('set a number...')
+
+  const doChange = (event) => {
+    setVal(event.target.value)
+  }
+
+  useEffect(() => {
+    let total = 0
+    for (let i = 0;i <= val;i++) {
+      total += i
+    }
+    setMsg("total: " + total + ".")
+  })
+
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage />
-        <CardMessage />
+        <AlertMessage msg={msg} />
+        <div className="form-group">
+          <label>Input:</label>
+          <input type="number" className="form-control" 
+              onChange={doChange} />
+        </div>
       </div>
     </div>
   )
