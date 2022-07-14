e84c908f373f5a80eb2c04462decf160bc2f576d
shintaro-mizumoto <82919755+shintaro-mizumoto@users.noreply.github.com>
2022-07-14 17:50:35 +0900

    4-4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react'
 import './App.css'
 
+function AlertMessage() {
+  return <div className="alert alert-primary h5 text-primary">
+    This is Alert message!
+  </div>
+}
+
+function CardMessage() {
+  return <div className="card p-3 h5 border-primary text-center">
+    This is Card message!
+  </div>
+}
+
 function App() {
-  const [count, setCount] = useState(0)
-  const clickFunc = () => {
-    setCount(count + 1)
-  }
-  
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <div className="alert alert-primary text-center">
-          <p className="h5 mb-3">click: {count} times!</p>
-          <div><button className="btn btn-primary"
-              onClick={clickFunc}>
-            Click me
-          </button></div>
-        </div>
+        <AlertMessage />
+        <CardMessage />
       </div>
     </div>
   )
