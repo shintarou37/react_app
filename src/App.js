4eac83cd35660438adad23eabc41cf1deeeb630e
shintaro-mizumoto <82919755+shintaro-mizumoto@users.noreply.github.com>
2022-07-15 19:00:44 +0900

    4-12

--- src/App.js
+++ src/App.js
@@ -8,21 +8,34 @@ function AlertMessage(props) {
 }
 
 function App() {
-  const [val, setVal] = useState(0)
-  const [msg, setMsg] = useState('set a number...')
+  const [val, setVal] = useState(1000)
+  const [tax1, setTax1] = useState(0)
+  const [tax2, setTax2] = useState(0)
+  const [msg, setMsg] = useState(<p>set a price...</p>)
 
   const doChange = (event) => {
     setVal(event.target.value)
   }
 
+  // ☆新たに追加したフック
   useEffect(() => {
-    let total = 0
-    for (let i = 0;i <= val;i++) {
-      total += i
-    }
-    setMsg("total: " + total + ".")
+    let res = <div>
+      <p>軽減税率(8%) ： {tax1} 円</p>
+      <p>通常税率(10%)： {tax2} 円</p>
+    </div>
+    setMsg(res)
+  }, [tax1, tax2])
+  
+  useEffect(() => {
+    console.log("2")
+    setTax1(Math.floor(val * 1.08))
   })
 
+  useEffect(() => {
+    console.log("3")
+    setTax2(Math.floor(val * 1.1))
+  })
+  
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
