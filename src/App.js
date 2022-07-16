8366790d4ae9314e18af5d3cfea7640196e4728e
shintarou37 <m.sintarou37@gmail.com>
2022-07-16 22:21:21 +0900

    4章さいご

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-import React, { useState, useEffect } from 'react'
 import './App.css'
-
-function AlertMessage(props) {
-  return <div className="alert alert-primary h5 text-primary">
-    <h5>{props.msg}</h5>
-  </div>
-}
+import MemoPage from './memo/MemoPage'
 
 function App() {
-  const [val, setVal] = useState(1000)
-  const [tax1, setTax1] = useState(0)
-  const [tax2, setTax2] = useState(0)
-  const [msg, setMsg] = useState(<p>set a price...</p>)
-
-  const doChange = (event) => {
-    setVal(event.target.value)
-  }
-
-  // ☆新たに追加したフック
-  useEffect(() => {
-    let res = <div>
-      <p>軽減税率(8%) ： {tax1} 円</p>
-      <p>通常税率(10%)： {tax2} 円</p>
-    </div>
-    setMsg(res)
-  }, [tax1, tax2])
-  
-  useEffect(() => {
-    console.log("2")
-    setTax1(Math.floor(val * 1.08))
-  })
-
-  useEffect(() => {
-    console.log("3")
-    setTax2(Math.floor(val * 1.1))
-  })
-  
   return (
     <div>
       <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
-        <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage msg={msg} />
-        <div className="form-group">
-          <label>Input:</label>
-          <input type="number" className="form-control" 
-              onChange={doChange} />
-        </div>
+        <h4 className="my-3">Memo.</h4>
+        <MemoPage />
       </div>
     </div>
   )
